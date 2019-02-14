859abb98cdf96ff90e0d12c695c7ae43b07db8ae
Alan Wolfe <alan.wolfe@gmail.com>
2019-02-13 20:08:57 -0800

    doc generation begin

--- docgen.lua
+++ docgen.lua
@@ -1,1 +1,35 @@
+function scandir(command)
+    local i, t, popen = 0, {}, io.popen
+    local pfile = popen(command)
+    for filename in pfile:lines() do
+        i = i + 1
+        t[i] = filename
+    end
+    pfile:close()
+    return t
+end
+
 print "=====Generating Documentation====="
+
+local sampleFamilies = scandir('cd ./src/samples/ && ls -d ./*/ && cd ../..')
+local testTypes = scandir('cd ./src/tests/ && ls -d ./*/ && cd ../..')
+
+local file = io.open("./src/toc.md", "w")
+
+file:write('# Table of Contents\n\n')
+
+file:write('## Samples\n\n')
+
+for k,v in pairs(sampleFamilies) do
+	local sampleFamily = string.sub(v,3,-2)
+	file:write('### '..sampleFamily..'\n\n')
+	local sampleTypes = scandir('cd ./src/samples/'..sampleFamily..'/ && ls -d ./*/ && cd ../../..')
+	for k2,v2 in pairs(sampleTypes) do
+		local sampleType = string.sub(v2,3,-2)
+		dofile("./src/samples/"..sampleFamily.."/"..sampleType.."/samples.lua")
+		-- [Regular Sampling](src/samples/_1d/regular/regular.md)
+		file:write('['..info.ShortName..'](src/samples/'..sampleFamily..'/'..sampleType..'/'..sampleType..'.md) - '..info.Description..'\n\n')
+	end
+end
+
+file:close()
---
+++ src/toc.md
@@ -0,0 +1,16 @@
+# Table of Contents
+
+## Samples
+
+### _1d
+
+[Blue Noise](src/samples/_1d/blue_noise/blue_noise.md) - Randomized sequences that have only high frequency content
+
+[Irrational Numbers](src/samples/_1d/irrational_numbers/irrational_numbers.md) - Multiply an irrational number by the squence index and take that mod 1 to make it be in [0,1)
+
+[LDS](src/samples/_1d/LDS/LDS.md) - Deterministic low discrepancy sequences
+
+[Regular](src/samples/_1d/regular/regular.md) - Uniformly spaced samples
+
+[Uniform Random](src/samples/_1d/uniform_random/uniform_random.md) - Randomized sample positions using a uniform random number distribution.
+
