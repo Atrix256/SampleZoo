17fadc074d0dae606b1a356e10afdad2da95e506
Alan Wolfe <awolfe@blizzard.com>
2019-02-15 12:53:41 -0800

    best candidate is progressive, so made the function honor that

--- src/samples/_1d/blue_noise/blue_noise.cpp
+++ src/samples/_1d/blue_noise/blue_noise.cpp
@@ -11,17 +11,23 @@ Description: Randomized sequences that have only high frequency content
 
 void Samples::_1d::BlueNoise::BestCandidate(std::vector<float>& values, size_t numValues)
 {
+    // if they want less samples than there are, just truncate the sequence
+    if (numValues <= values.size())
+    {
+        values.resize(numValues);
+        return;
+    }
+
     static const size_t c_blueNoiseSampleMultiplier = 1;
 
     // make sure we use "the good stuff". yes, it matters if the not good stuff gets in, it's a big difference.
     // https://blog.demofox.org/2017/03/15/neural-network-recipe-recognize-handwritten-digits-with-95-accuracy/
-    std::random_device rd("dev/random");
-    std::mt19937 rng(rd());
-    std::uniform_real_distribution<float> dist(0, 1);
+    static std::random_device rd("dev/random");
+    static std::mt19937 rng(rd());
+    static std::uniform_real_distribution<float> dist(0, 1);
 
-    values.resize(numValues);
-    size_t sampleInt = 0;
-    for (size_t i = 0; i < numValues; ++i)
+    // use whatever samples currently exist, and just add to them, since this is a progressive sequence
+    for (size_t i = values.size(); i < numValues; ++i)
     {
         size_t numCandidates = values.size() * c_blueNoiseSampleMultiplier + 1;
         float bestDistance = 0.0f;
@@ -45,6 +51,6 @@ void Samples::_1d::BlueNoise::BestCandidate(std::vector<float>& values, size_t n
                 bestCandidateValue = candidateValue;
             }
         }
-        values[i] = bestCandidateValue;
+        values.push_back(bestCandidateValue);
     }
 }
