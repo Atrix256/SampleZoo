93de24742afc9a3b7d13ccb54cc066ea34700878
Alan Wolfe <awolfe@blizzard.com>
2019-02-15 12:56:14 -0800

    uniform random numbers are progressive too

--- src/samples/_1d/uniform_random/uniform_random.cpp
+++ src/samples/_1d/uniform_random/uniform_random.cpp
@@ -9,13 +9,19 @@ DATE: 2/10/2019
 
 void Samples::_1d::UniformRandom::UniformRandom(std::vector<float>& values, size_t numValues)
 {
+    // if they want less samples than there are, just truncate the sequence
+    if (numValues <= values.size())
+    {
+        values.resize(numValues);
+        return;
+    }
+
     // make sure we use "the good stuff". yes, it matters if the not good stuff gets in, it's a big difference.
     // https://blog.demofox.org/2017/03/15/neural-network-recipe-recognize-handwritten-digits-with-95-accuracy/
-    std::random_device rd("dev/random");  
-    std::mt19937 rng(rd());
-    std::uniform_real_distribution<float> dist(0, 1);
+    static std::random_device rd("dev/random");
+    static std::mt19937 rng(rd());
+    static std::uniform_real_distribution<float> dist(0, 1);
 
-    values.resize(numValues);
-    for (size_t i = 0; i < numValues; ++i)
-        values[i] = dist(rng);
+    for (size_t i = values.size(); i < numValues; ++i)
+        values.push_back(dist(rng));
 }
