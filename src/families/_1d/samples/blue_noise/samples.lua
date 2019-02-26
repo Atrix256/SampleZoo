fdb8fcde54e92f25aab3f29c88c39a869997d68f
Alan Wolfe <alan.wolfe@gmail.com>
2019-02-25 21:15:24 -0800

    best candidate refined not progressive

--- src/families/_1d/samples/blue_noise/samples.lua
+++ src/families/_1d/samples/blue_noise/samples.lua
@@ -5,6 +5,6 @@ sampleInfo = {
     Description="Randomized sequences that have only high frequency content",
     Functions= {
         { name = "BestCandidate", progressive = true, randomized = true, cache = true },
-        { name = "BestCandidateRefined", progressive = true, randomized = true, cache = true },
+        { name = "BestCandidateRefined", progressive = false, randomized = true, cache = true },
     },
 }
